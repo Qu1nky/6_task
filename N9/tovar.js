efe8f28d067b84867eab34993cba8d6d06a786c4
Alexandr <alex.rakitin2017@mail.ru>
2024-12-12 03:55:16 +0300

    Добавлена информация о товарах

---
+++ N9/tovar.js
@@ -0,0 +1,41 @@
+export let tovarObj = [
+    {
+        nameTovar: "Фитнес-браслет DEXP SB60",
+        imgSrc: "1.jpg",
+        corpus: "Черный",
+        remen: "Черный",
+        displaySize: "0,96\"",
+        displayType:"OLED",
+        bluetooth: true,
+        OS:{
+            os1:"Android 4.2",
+            os2:"iOS 8",
+        },
+    },
+    {
+        nameTovar: "Смарт-часы RunGo W2",
+        imgSrc: "2.jpg",
+        corpus: "Зеленый",
+        remen: "Зеленый",
+        displaySize: "1,3\"",
+        displayType:"TFT (TN)",
+        bluetooth: true,
+        OS:{
+            os1:"Android",
+            os2:"iOS",
+        },
+    },
+    {
+        nameTovar: "Смарт-часы Wellbe Prime",
+        imgSrc: "3.jpg",
+        corpus: "Черный",
+        remen: "Розовый",
+        displaySize: "1,69\"",
+        displayType:"IPS",
+        bluetooth: true,
+        OS:{
+            os1:"Android 5.0",
+            os2:"IOS 9",
+        }
+    }
+]
